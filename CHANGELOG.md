bd438eab601f30d54fece7b9773866675b1732d4
Raphaël Munoz <raphael@aprobado.ch>
2026-03-31 10:04:43 +0200

    push for 1st day course v 1.0.0 - changes in log

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,20 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.0.0] - 2026-03-31
+
+### Added
+
+- A "Super Slow Area" prefab example
+
+### Changed
+
+- Readme has been fixed and updated
+- "Super Jump Area" example value has been exaggerated
+- PlayerStatsSetter.cs can modify stats to FirstPersonController AND ThirdPersonController
+- FirstPersonCamera tweaking
+- Decreased mouse look sensibility
+
 ## [0.1.0] - 2026-02-16
 
 ### This is the first release of *\<Narrations Jouables 2026\>*.
--- Runtime/Scripts/PlayerStatsSetter.cs
+++ Runtime/Scripts/PlayerStatsSetter.cs
@@ -5,25 +5,44 @@ namespace NarrationsJouables
 {
     public class PlayerStatsSetter : MonoBehaviour
     {
-        private FirstPersonController controller;
-
-        private FirstPersonController Controller
+        private FirstPersonController firstPersonController;
+        private FirstPersonController FirstPersonController
         {
             get
             {
-                if (controller == null) controller = FindFirstObjectByType<FirstPersonController>();
-                return controller;
+                if (firstPersonController == null)
+                    firstPersonController = FindFirstObjectByType<FirstPersonController>(FindObjectsInactive.Include);
+                return firstPersonController;
+            }
+        }
+        
+        private ThirdPersonController thirdPersonController;
+        private ThirdPersonController ThirdPersonController
+        {
+            get
+            {
+                if (thirdPersonController == null)
+                    thirdPersonController = FindFirstObjectByType<ThirdPersonController>(FindObjectsInactive.Include);
+                return thirdPersonController;
             }
         }
 
         public void SetJumpHeight(float jumpHeight)
         {
-            if (Controller != null) Controller.JumpHeight = jumpHeight;
+            if (FirstPersonController != null) FirstPersonController.JumpHeight = jumpHeight;
+            if (ThirdPersonController != null) ThirdPersonController.JumpHeight = jumpHeight;
         }
 
         public void SetMoveSpeed(float moveSpeed)
         {
-            if (Controller != null) Controller.MoveSpeed = moveSpeed;
+            if (FirstPersonController != null) FirstPersonController.MoveSpeed = moveSpeed;
+            if (ThirdPersonController != null) ThirdPersonController.MoveSpeed = moveSpeed;
+        }
+        
+        public void SetSprintSpeed(float sprintSpeed)
+        {
+            if (FirstPersonController != null) FirstPersonController.SprintSpeed = sprintSpeed;
+            if (ThirdPersonController != null) ThirdPersonController.SprintSpeed = sprintSpeed;
         }
     }
 }
