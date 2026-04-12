7fdd55e7e432fdb1ea08a2bf345a7568c331ecc3
Raphaël Munoz <raphael@aprobado.ch>
2026-04-12 16:02:50 +0200

    title screen template added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,9 +4,15 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-## ]1.0.1] - 2026-03-31
+## [Unreleased]
 
-## Changed
+### Added
+
+- Title screen template scene, prefab and scripts
+
+## [1.0.1] - 2026-03-31
+
+### Changed
 
 - Fixed version number in package.json
 
---
+++ Runtime/Scripts/TitleScreen/LevelLoader.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NarrationsJouables.TitleScreen
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        public string levelNameToLoad;
+        public int levelIndexToLoad = -1;
+
+        private bool loading = false;
+
+        public void LoadLevelByName()
+        {
+            StartCoroutine(LoadCoroutine(levelNameToLoad, -1));
+        }
+
+        public void LoadLevelByID()
+        {
+            StartCoroutine(LoadCoroutine("", levelIndexToLoad));
+        }
+
+        public void LoadLevel(string levelName)
+        {
+            StartCoroutine(LoadCoroutine(levelName, -1));
+        }
+
+        public void LoadLevel(int levelIndex)
+        {
+            StartCoroutine(LoadCoroutine("", levelIndex));
+        }
+
+        IEnumerator LoadCoroutine(string levelName, int levelIndex)
+        {
+            if (levelIndex < 0 && string.IsNullOrWhiteSpace(levelName)) yield break; // no valid scene name or index
+            if (loading) yield break;   // don't load if already loading
+            
+            loading = true;
+            yield return null;  // wait to let any visual change to happen before loading
+            Application.backgroundLoadingPriority = ThreadPriority.High;
+
+            if (levelIndex >= 0)
+            {
+                var asyncOp = SceneManager.LoadSceneAsync(levelIndex);
+                asyncOp.completed += NormalLoadPriority;
+            }
+            else
+            {
+                var asyncOp = SceneManager.LoadSceneAsync(levelName);
+                asyncOp.completed += NormalLoadPriority;
+            }
+        }
+
+        private void NormalLoadPriority(AsyncOperation obj)
+        {
+            Application.backgroundLoadingPriority = ThreadPriority.Normal;
+        }
+    }
+}
